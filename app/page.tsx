657fb7e188349021ac3c9672daf4f12def1dd25d
ShahzadTabassum <shahzadt481@gmail.com>
2023-11-30 02:02:02 +0500

    add assignments file

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,68 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className='body-page'>
+      <section className='header'>
+        <h1 className='project-name'>
+        Certified Web 3.0 and Metaverse Developer: A Nationwide Program in Karachi, Lahore, Islamabad, and Faisalabad
+        </h1>
+        <h2 className='project-tagline'>Getting Ready for the Next Generation and Future of the Internet - Join a 13 Trillion Dollar Industry with 5 Billion Users</h2>
+        </section>
+        <section className='main-page'>
+          <h1 className='main-content'>Certified Web 3.0 and Metaverse Developer: A Nationwide Program in Karachi, Lahore, Islamabad, and Faisalabad</h1>
+          <img src='logo.png' alt='logo-type'/>
+          <h1 className='future-dic'>The Future of the Web is Web 3.0, Metaverse, and Edge Computing. Panaverse DAO is a movement to spread these technolgies globally. It is community of Web 3 and Metaverse developers, designers, trainers, startup founders and service providers.</h1>
+          <h2 className='piaic-page'><a href="https://www.piaic.org/">Admissions Now Open in Karachi, Lahore, Islamabad, and Faisalabad</a></h2>
+          <div className='new-div'>
+          <h1 className='headings'><a href="https://docs.google.com/document/d/13Z4tMKQmZpMEsOc1Y_qAXbOstRolGIFupHxQFgAFl98/edit">This document on Google Docs</a></h1>
+          <p className='p'>The internet is without a doubt the most important technological development in human history. Web3 and metaverse technologies expand the internet as we know it by introducing novel features and advancements. Metaverse will make use of all aspects of modern technology, including 3D, VR, AR, AI, blockchain, cloud computing, voice computing, ambient computing, and more.</p>
+          <p className='p'>Citi is the latest Wall Street business to give a positive prognosis for Web 3.0 and the Metaverse, terms used to depict a future internet vision centred on decentralised technologies and virtual worlds. Citi stated in a March 2022 <a href='https://www.citigroup.com/global'>research paper</a> that the metaverse economy might have a total addressable market of up to $13 trillion and five billion people by 2030.</p>
+          <h1 className='headings'>The Program in a Nutshell: Earn While You Learn</h1>
+          <p className='p'>In this brand-new type of curriculum, students will learn how to make money and boost exports in the classroom and will begin doing so within six months of the program’s beginning. It resembles a cross between a corporate venture and an educational project.</p>
+          <h1 className='headings'>Program of Studies</h1>
+          <p className='p'>This curriculum is intended for beginners who want to learn software development from the ground up. The first two quarters are shared by all specialities and are dedicated to studying Object-Oriented Programming and cutting-edge Full-Stack Web 2.0 development. It is going to be a year-long hybrid programme that includes both onsite and online classes and is divided into four quarters of 13 weeks each. The emphasis will be on hands-on learning by educating students to produce projects. To accommodate everyone, courses will be held primarily on weekends or after 6:00 p.m. (Pakistan Time) on weekdays. It employs a hybrid teaching format, with core onsite classes complemented by online Zoom laboratories and recorded videos.</p>
+          <h1 className='headings'>Core Courses (Common in All Specializations)</h1>
+          <p className='p'>Every participant of the program will start by completing the following two core courses:</p>
+          <p className='p'><a href='https://github.com/panaverse/panaverse.github.io#quarter-i-core'>Quarter I (Core) CS-101: Object-Oriented Programming using TypeScript</a></p>
+          <p className='p'><a href='https://github.com/panaverse/panaverse.github.io#quarter-ii-core'>Quarter II (Core) W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Cloud Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for Terraform</a></p>
+          <h1 className='headings'>Specialized Tracks</h1>
+          <p className='p'>After completing the first two quarters the participants will select one or more specializations consisting of two courses each:</p>
+          <h1 className='headings'>Web 3.0 (Blockchain) and Metaverse Specialization</h1>
+          <p className='p'>This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.</p>
+          <p className='p'><a href='https://www.panaverse.co/'>Quarter III W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps</a></p>
+          <p className='p'><a href='https://www.panaverse.co/'>Quarter IV MV-361: Developing Planet-Scale Open Virtual and Augmented Metaverse Experiences</a></p>
+          <h1 className='headings'>Artificial Intelligence (AI) and Deep Learning Specialization</h1>
+          <p className='p'>The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.</p>
+          <p className='p'><a href='https://www.panaverse.co/'>Quarter III AI-351: Developing Planet-Scale Intelligent APIs and Python Programming</a></p>
+          <p className='p'><a href='https://www.panaverse.co/'>Quarter IV AI-361: Deep Learning and MLOps</a></p>
+          <h1 className='headings'>Cloud-Native Computing Specialization</h1>
+          <p className='p'>The Cloud-Native Computing Specialization focuses on Containers, Kubernetes, and CDK for Kubernetes.</p>
+          <p className='p'><a href='https://www.panaverse.co/'>Quarter III CN-351: Certified Kubernetes Application Developer (CKAD)</a></p>
+          <p className='p'><a href='https://www.panaverse.co/'>Quarter IV CN-361: Developing Multi-Cloud APIs using CDK for Terraform</a></p>
+          <h1 className='headings'>Ambient Computing and IoT Specialization</h1>
+          <p className='p'>The Ambient Computing and IoT Specialization focuses on building Smart Homes, Offices, Factories, and Cities using Voice computing, Matter and Embedded Devices.</p>
+          <p className='p'><a href='https://www.panaverse.co'>Quarter III AC-351: Ambient Computing with Voice Assistants and Matter Devices</a></p>
+          <p className='p'><a href='https://www.panaverse.co'>Quarter IV AC-361: Embedded Programming using C and Rust</a></p>
+          <h1 className='headings'>The Outcome for Participants of the Program</h1>
+          <p className='p'>The graduates of this program will own products (Full-Stack App Templates, AR and VR Experiences, and APIs) that are marketed globally by the Panaverse DAO and, if they like, will also be able to start off by offering services at a rate of $50 per hour ($96,000 per year). This would give Pakistani professionals and students a fantastic opportunity to better their financial situation while also giving the economy a much-needed boost by expanding software exports.</p>
+          <p className='p'><a href='https://content.techgig.com/hiring/top-5-metaverse-jobs-that-will-rule-the-future-of-tech-industry/articleshow/87581325.cms'>Top 5 ‘Metaverse’ jobs that will rule the future of tech industry</a></p>
+          <p className='p'><a href='https://web3.career/web3-salaries/blockchain-developer'>Blockchain Developer Salary - Jun 2022</a></p>
+          <p className='p'><a href='https://thedefiant.io/web3-soaring-salaries'>Web3 Salaries Soar to $750,000 for Rank-and-File Devs</a></p>
+          <p className='p'><a href='https://newzoo.com/resources/trend-reports/newzoo-report-on-metaverse-blockchain-gaming-nft-2022'>The Metaverse, Blockchain Gaming, and NFTs: Navigating the Internet’s Uncharted Waters</a></p>
+          <p className='p'><a href='https://www.blockchain-council.org/metaverse/how-to-become-metaverse-developer/'>How To Become Metaverse Developer: Scope, Skills, and Salary</a></p>
+          <h1 className='headings'>Relevant Information Links</h1>
+          <p className='p'><a href='https://www.piaic.org/'>Admission Website</a></p>
+          <p className='p'><a href='https://www.facebook.com/groups/panaverse'>Facebook Group</a></p>
+          <p className='p'><a href='https://www.youtube.com/@panaverse/streams'>YouTube Live Channel</a></p>
+          <p className='p'><a href='https://twitter.com/Panaverse_edu'>Twitter</a></p>
+          <p className='p'><a href='https://github.com/panaverse'>Github Repos</a></p>
+          </div>
+          </section>
+          <section>
+            <div>
+            <h1></h1>
+            </div>
+          </section>
     </main>
   )
 }
